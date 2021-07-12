9fd4cf1ad70d482391d12caedd79ec1ff568aa8d
celiaxiao <c6xiao@ucsd.edu>
2021-07-13 00:20:11 +0800

    incorporate selection into app.tsx

--- ubiquitous-viz-server/uv-frontend/src/App.tsx
+++ ubiquitous-viz-server/uv-frontend/src/App.tsx
@@ -7,6 +7,9 @@ import SelectionModalForm from "./components/selection_modal_form"
 
 //To Delete
 import { tableNameEnum, columnNameEnum } from "./components/temp"
+import { Button, Tabs } from "antd"
+import { PageContainer } from "@ant-design/pro-layout"
+import TabPane from "@ant-design/pro-card/lib/components/TabPane"
 
 function App() {
   return (
@@ -23,8 +26,28 @@ function App() {
           deleteConn={deleteConn}
         />
       </div> */}
-      <div className="App-body">
-        <SelectionModalForm tableNameEnum={tableNameEnum} columnNameEnum={columnNameEnum} />
+      <div >
+        <Tabs style={{ margin: "10px" }}>
+          <TabPane tab="Database Configation" key="db_config">
+            <div className="App-body" >
+              <DatabaseConfiguration
+                checkConnection={checkConnection}
+                listConn={listConn}
+                createConn={createConn}
+                updateConn={updateConn}
+                deleteConn={deleteConn}
+              />
+            </div>
+          </TabPane>
+          <TabPane tab="Selection" key="select">
+            <div className="App-body" >
+              <SelectionModalForm key='select' tableNameEnum={tableNameEnum} columnNameEnum={columnNameEnum} />
+            </div>
+          </TabPane>
+
+
+        </Tabs>
+        {/* <SelectionModalForm tableNameEnum={tableNameEnum} columnNameEnum={columnNameEnum} /> */}
       </div>
       <div className="App-footer">
         <a
