9e2fd2bac0ff56f11c1559a21d435b73a5a819a6
Jacob <jacobbishopxy@gmail.com>
2021-09-10 00:37:54 +0800

    mnt

--- tiny-df/src/db/loader.rs
+++ tiny-df/src/db/loader.rs
@@ -2,113 +2,97 @@
 //!
 //! Similar to Python's pandas dataframe: `pd.Dataframe.to_sql`
 
-use std::fmt::Display;
-
 use async_trait::async_trait;
-use sqlx::mysql::MySqlRow;
-use sqlx::{
-    Connection, MySqlConnection, MySqlPool, PgConnection, PgPool, SqliteConnection, SqlitePool,
-};
+use sqlx::mysql::{MySqlColumn, MySqlRow};
+use sqlx::postgres::{PgColumn, PgRow};
+use sqlx::sqlite::{SqliteColumn, SqliteRow};
+use sqlx::{MySqlPool, PgPool, Row, SqlitePool};
 
-use crate::db::TdDbResult;
+use crate::db::{ConnInfo, TdDbError, TdDbResult};
 use crate::prelude::*;
 use crate::se::Sql;
 
-use super::TdDbError;
+/// Loader's engine
+/// fetching data from database
+#[async_trait]
+pub trait Engine {
+    async fn fetch_all(&self, query: &str) -> TdDbResult<Option<Dataframe>>;
 
-pub struct ConnInfo {
-    pub driver: Sql,
-    pub username: String,
-    pub password: String,
-    pub host: String,
-    pub port: i32,
-    pub database: String,
+    // async fn insert(&self, dataframe: Dataframe) -> TdDbResult<()>;
+
+    // async fn update(&self, dataframe: Dataframe) -> TdDbResult<()>;
+
+    // async fn upsert(&self, dataframe: Dataframe) -> TdDbResult<()>;
 }
 
-impl ConnInfo {
-    pub fn new(
-        driver: Sql,
-        username: &str,
-        password: &str,
-        host: &str,
-        port: i32,
-        database: &str,
-    ) -> ConnInfo {
-        ConnInfo {
-            driver,
-            username: username.to_owned(),
-            password: password.to_owned(),
-            host: host.to_owned(),
-            port,
-            database: database.to_owned(),
-        }
+// TODO:
+impl From<MySqlColumn> for DataframeData {
+    fn from(v: MySqlColumn) -> Self {
+        v.into()
     }
 }
 
-impl Display for ConnInfo {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}://{}:{}@{}:{}/{}",
-            self.driver, self.username, self.password, self.host, self.port, self.database,
-        )
+impl From<PgColumn> for DataframeData {
+    fn from(v: PgColumn) -> Self {
+        v.into()
     }
 }
 
-pub enum TdConnection {
-    Mysql(MySqlConnection),
-    Postgres(PgConnection),
-    Sqlite(SqliteConnection),
+impl From<SqliteColumn> for DataframeData {
+    fn from(v: SqliteColumn) -> Self {
+        v.into()
+    }
 }
 
-pub enum TdPool {
-    Mysql(MySqlPool),
-    Postgres(PgPool),
-    Sqlite(SqlitePool),
+#[async_trait]
+impl Engine for MySqlPool {
+    async fn fetch_all(&self, query: &str) -> TdDbResult<Option<Dataframe>> {
+        let mut d2 = sqlx::query(query)
+            .map(|row: MySqlRow| {
+                // map
+                let cols: &[sqlx::mysql::MySqlColumn] = row.columns();
+                todo!()
+            })
+            .fetch_all(self)
+            .await?;
+        todo!()
+    }
+}
+
+#[async_trait]
+impl Engine for PgPool {
+    async fn fetch_all(&self, query: &str) -> TdDbResult<Option<Dataframe>> {
+        let mut d2 = sqlx::query(query)
+            .map(|row: PgRow| {
+                // map
+                let cols = row.columns();
+                todo!()
+            })
+            .fetch_all(self)
+            .await?;
+        todo!()
+    }
+}
+
+#[async_trait]
+impl Engine for SqlitePool {
+    async fn fetch_all(&self, query: &str) -> TdDbResult<Option<Dataframe>> {
+        let mut d2 = sqlx::query(query)
+            .map(|row: SqliteRow| {
+                // map
+                let cols = row.columns();
+                todo!()
+            })
+            .fetch_all(self)
+            .await?;
+        todo!()
+    }
 }
 
 pub struct Loader {
     driver: Sql,
     conn: String,
-    pool: Option<TdPool>,
-}
-
-#[async_trait]
-pub trait TdConnTrait {
-    async fn connect(&self) -> TdDbResult<TdConnection>;
-}
-
-#[async_trait]
-pub trait TdPoolTrait {
-    async fn pool(&self) -> TdDbResult<TdPool>;
-}
-
-#[async_trait]
-impl TdConnTrait for Loader {
-    async fn connect(&self) -> TdDbResult<TdConnection> {
-        match self.driver {
-            Sql::Mysql => Ok(TdConnection::Mysql(
-                MySqlConnection::connect(&self.conn).await?,
-            )),
-            Sql::Postgres => Ok(TdConnection::Postgres(
-                PgConnection::connect(&self.conn).await?,
-            )),
-            Sql::Sqlite => Ok(TdConnection::Sqlite(
-                SqliteConnection::connect(&self.conn).await?,
-            )),
-        }
-    }
-}
-
-#[async_trait]
-impl TdPoolTrait for Loader {
-    async fn pool(&self) -> TdDbResult<TdPool> {
-        match self.driver {
-            Sql::Mysql => Ok(TdPool::Mysql(MySqlPool::connect(&self.conn).await?)),
-            Sql::Postgres => Ok(TdPool::Postgres(PgPool::connect(&self.conn).await?)),
-            Sql::Sqlite => Ok(TdPool::Sqlite(SqlitePool::connect(&self.conn).await?)),
-        }
-    }
+    pool: Option<Box<dyn Engine>>,
 }
 
 // TODO: transaction is required
@@ -137,14 +121,10 @@ impl Loader {
     }
 
     // TODO:
-    pub async fn tmp_query(&self, query: &str) -> TdDbResult<Option<Dataframe>> {
-        match self.connect().await {
-            Ok(c) => match c {
-                TdConnection::Mysql(c) => todo!(),
-                TdConnection::Postgres(c) => todo!(),
-                TdConnection::Sqlite(c) => todo!(),
-            },
-            Err(e) => Err(e),
+    pub async fn fetch_all(&self, query: &str) -> TdDbResult<Option<Dataframe>> {
+        match &self.pool {
+            Some(p) => Ok(p.fetch_all(query).await?),
+            None => Err(TdDbError::Common("Loader pool not set".to_owned())),
         }
     }
 }
--- tiny-df/src/db/mod.rs
+++ tiny-df/src/db/mod.rs
@@ -1,4 +1,49 @@
+//！Db: tiny-df database interaction module
+//！
+//！data conversion
+
 pub mod errors;
 pub mod loader;
 
 pub use errors::{TdDbError, TdDbResult};
+
+use crate::se::Sql;
+
+pub struct ConnInfo {
+    pub driver: Sql,
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub port: i32,
+    pub database: String,
+}
+
+impl ConnInfo {
+    pub fn new(
+        driver: Sql,
+        username: &str,
+        password: &str,
+        host: &str,
+        port: i32,
+        database: &str,
+    ) -> ConnInfo {
+        ConnInfo {
+            driver,
+            username: username.to_owned(),
+            password: password.to_owned(),
+            host: host.to_owned(),
+            port,
+            database: database.to_owned(),
+        }
+    }
+}
+
+impl std::fmt::Display for ConnInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}://{}:{}@{}:{}/{}",
+            self.driver, self.username, self.password, self.host, self.port, self.database,
+        )
+    }
+}
